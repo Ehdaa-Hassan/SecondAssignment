f6cc85ab20c938aacdf7aea85af0f636ffeebeb8
Ehdaa Hassan <ehdaahassan97@gmail.com>
2019-10-26 12:10:28 +0200

    Second User Interface - Your Profile

---
+++ app/src/main/java/com/example/android/profile/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.android.profile;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.drawable.RoundedBitmapDrawable;
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try
+        {
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException e){}
+
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
